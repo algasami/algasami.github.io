907a5eb3d165703dbced883485e987f422cc6630
algasami <105358814+algasami@users.noreply.github.com>
2022-09-30 22:40:44 +0800

    test name change

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,25 +1,26 @@
 module.exports = {
-  apps: [
-    {
-      name: "app",
-      script: "npm run build && npm run start",
-      env: {
-        NODE_ENV: "development"
-      },
-      env_production: {
-        NODE_ENV: "production"
-      }
-    }
-  ],
-  deploy: {
-    production: {
-      host: '172.104.39.247',
-      user: 'algasami',
-      ref: 'origin/master', // (use 'origin/master' for your master branch,
-      repo: "git@github.com:algasami/portfolio_v1.git", // your repo url
-      path: "~/dev-test",
-      "post-deploy":
-        "npm install && pm2 reload ecosystem.config.js --env production && pm2 save"
-    }
-  }
+	apps: [
+		{
+			name: "app",
+			script: "npm run build && npm run start",
+			env: {
+				NODE_ENV: "development",
+			},
+			env_production: {
+				NODE_ENV: "production",
+			},
+		},
+	],
+	deploy: {
+		production: {
+			host: "172.104.39.247",
+			key: "~/.ssh/id_rsa",
+			user: "algasami",
+			ref: "origin/master", // (use 'origin/master' for your master branch,
+			repo: "git@github.com:algasami/portfolio_v1.git", // your repo url
+			path: "/home/algasami/dev-test/",
+			"post-deploy":
+				"npm install && pm2 reload ecosystem.config.js --env production && pm2 save",
+		},
+	},
 };
