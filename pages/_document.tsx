38e210ace48a489dcd2074b14d8fac16579c9dd3
algasami <105358814+algasami@users.noreply.github.com>
2023-02-10 23:04:33 +0800

    static render

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -12,6 +12,35 @@ export default function Document() {
 					rel="stylesheet"
 					href="https://fonts.googleapis.com/icon?family=Material+Icons"
 				/>
+				<meta name="title" content="Algasami" />
+				<meta
+					name="description"
+					content="Hello there. I'm Algasami, a programmer, game developer and web developer"
+				/>
+
+				<meta property="og:type" content="website" />
+				<meta property="og:url" content="https://www.algasami.com/" />
+				<meta property="og:title" content="Algasami" />
+				<meta
+					property="og:description"
+					content="Hello there. I'm Algasami, a programmer, game developer and web developer"
+				/>
+				<meta
+					property="og:image"
+					content="https://i.pinimg.com/originals/f2/69/b8/f269b8bab77f4224428c10df537d0340.jpg"
+				/>
+
+				<meta property="twitter:card" content="summary_large_image" />
+				<meta property="twitter:url" content="https://www.algasami.com/" />
+				<meta property="twitter:title" content="Algasami" />
+				<meta
+					property="twitter:description"
+					content="Hello there. I'm Algasami, a programmer, game developer and web developer"
+				/>
+				<meta
+					property="twitter:image"
+					content="https://i.pinimg.com/originals/f2/69/b8/f269b8bab77f4224428c10df537d0340.jpg"
+				/>
 			</Head>
 			<body>
 				<Main />
