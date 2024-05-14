ef3e6ca832425657a68f39ad0b4db32a41bec56e
algasami <105358814+algasami@users.noreply.github.com>
2024-05-14 08:15:30 +0800

    switches icon

--- app/components/langSwitch/langSwitch.tsx
+++ app/components/langSwitch/langSwitch.tsx
@@ -1,3 +1,5 @@
+import { Translate } from "@mui/icons-material";
+import { Icon, IconButton } from "@mui/material";
 import { Locale } from "i18n-config";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
@@ -14,22 +16,14 @@ const LangSwitch = ({ lang }: { lang: Locale }) => {
   arr[0] = next;
 
   return (
-    <Link
-      locale={next}
+    <IconButton
       href={"/" + arr.join("/")}
       aria-label="Toggle Language (zh-tw/en)"
+      color="inherit"
       type="button"
-      className="h-10 w-10 rounded p-2 transition-colors hover:bg-gray-100 dark:hover:bg-gray-800 sm:h-12 sm:w-12 sm:p-3"
     >
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 -960 960 960"
-        fill={"currentColor"}
-        className="text-gray-900 transition-colors dark:text-gray-100"
-      >
-        <path d="m475-80 185-480h79L924-80h-65l-45-117H584L539-80h-64ZM162-201l-42-42 201-201q-51-53-85.5-107.5T183-660h65q16 43 43.5 85t72.5 88q46-48 85-117.5T505-740H40v-60h290v-80h60v80h290v60H567q-17 78-61.5 159.5T406-443l102 104-24 63-121-125-201 200Zm443-51h188l-94-248-94 248Z" />
-      </svg>
-    </Link>
+      <Translate fontSize="large" />
+    </IconButton>
   );
 };
 
--- app/components/themeSwitch.tsx
+++ app/components/themeSwitch.tsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
 import { useThemeHook } from "../hooks/themeHook";
+import { IconButton } from "@mui/material";
+import { DarkModeOutlined, LightModeOutlined } from "@mui/icons-material";
 
 const ThemeSwitch = () => {
   const [mounted, setMounted] = useState(false);
@@ -11,29 +13,17 @@ const ThemeSwitch = () => {
   const isDark = theme === "dark";
 
   return (
-    <button
-      aria-label="Toggle Dark Mode"
-      type="button"
-      className="h-10 w-10 rounded p-2 transition-colors hover:bg-gray-100 dark:hover:bg-gray-800 sm:h-12 sm:w-12 sm:p-3"
+    <IconButton
+      aria-label="navigate"
+      color="inherit"
       onClick={() => setTheme(isDark ? "light" : "dark")}
     >
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 20 20"
-        fill="currentColor"
-        className="text-gray-900 transition-colors dark:text-gray-100"
-      >
-        {mounted && isDark ? (
-          <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z" />
-        ) : (
-          <path
-            fillRule="evenodd"
-            d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z"
-            clipRule="evenodd"
-          />
-        )}
-      </svg>
-    </button>
+      {isDark ? (
+        <DarkModeOutlined fontSize="large" />
+      ) : (
+        <LightModeOutlined fontSize="large" />
+      )}
+    </IconButton>
   );
 };
 
